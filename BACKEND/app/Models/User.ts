2e69692b8ccfac2c47dfdbe67079e24837e13893
ciceroff <cicero.barrozo13@gmail.com>
2022-01-04 16:26:25 -0300

    feat: create users model

---
+++ BACKEND/app/Models/User.ts
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeSave, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import Role from './Role'
+import  Hash  from '@ioc:Adonis/Core/Hash'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public cpfNumber: string
+  
+  @column()
+  public email: string
+
+  @column()
+  public password: string
+
+  @column()
+  public isAproved: boolean | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @manyToMany(()=> Role, {
+    pivotTimestamps: true, 
+    pivotTable: 'user_roles'
+  })
+  public roles: ManyToMany<typeof Role>
+
+  @beforeSave()
+  public static async hashPassword(user: User){
+    if(user.$dirty.password){
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
